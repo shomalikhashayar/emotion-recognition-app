b5727294abb251911ad5470ac96ebd75d4511082
Khashayar Shomali <shomalikhashayar@gmail.com>
2024-10-07 09:49:39 +0330

    LoginPage UI.

--- src/components/LoginPage/DetailSection.vue
+++ src/components/LoginPage/DetailSection.vue
@@ -1,22 +1,38 @@
 <template>
-  <q-card-section class="q-pa-xl col column bg-white">
-    <div class="text-h3 text-center text-weight-700">ورود</div>
+  <q-card-section class="q-pa-xl q-ma-md col-6 column bg-white">
+    <div class="text-h3 text-center text-weight-700 no-letter-spacing">
+      ورود
+    </div>
     <div class="q-mb-md">
-      <div class="q-mb-sm text-body1 text-grey-8">ایمیل یا نام کاربری</div>
-      <q-input outlined rounded v-model="userName" />
+      <div class="q-mb-sm text-body1 text-grey-8 no-letter-spacing">
+        ایمیل یا نام کاربری
+      </div>
+      <q-input outlined rounded v-model="username">
+        <template #append>
+          <q-icon color="primary" name="mail" class="q-mr-sm" />
+        </template>
+      </q-input>
     </div>
     <div class="q-mb-lg">
-      <div class="q-mb-sm text-body1 text-grey-8">رمز عبور</div>
-      <q-input outlined rounded v-model="password" />
+      <div class="q-mb-sm text-body1 text-grey-8 no-letter-spacing">
+        رمز عبور
+      </div>
+      <q-input outlined rounded v-model="password">
+        <template #append>
+          <q-icon color="primary" name="key" class="q-mr-sm" />
+        </template>
+      </q-input>
     </div>
-    <div class="text-body2 text-weight-500 text-red-6">
+    <router-link
+      class="no-decoration decoration-on-hover text-body2 text-weight-500 text-primary"
+    >
       رمز عبورتان را فراموش کرده‌اید؟
-    </div>
+    </router-link>
 
     <q-btn
       rounded
       unelevated
-      color="orange-8"
+      color="primary"
       class="full-weight q-pa-md q-mt-xl"
     >
       <span class="text-body1 text-weight-700">ورود</span>
@@ -24,7 +40,10 @@
 
     <div class="q-mt-lg text-body2 text-grey-8">
       حساب کاربری ندارید؟
-      <span class="text-body2 text-weight-700 text-orange-8">ثبت نام</span>
+      <router-link
+        class="text-body2 text-weight-700 text-primary no-decoration decoration-on-hover"
+        >ثبت نام</router-link
+      >
     </div>
   </q-card-section>
 </template>
@@ -32,6 +51,6 @@
 <script setup>
 import { ref } from "vue";
 
-const userName = ref(null);
+const username = ref(null);
 const password = ref(null);
 </script>
--- src/components/LoginPage/MasterSection.vue
+++ src/components/LoginPage/MasterSection.vue
@@ -1,9 +1,10 @@
 <template>
-  <q-card-section class="col master-section flex flex-center">
-    <q-avatar class="logo" size="200px">
-      <q-img width="80px" src="icons/face-id.svg" />
+  <q-card-section class="col-6 master-section flex flex-center no-padding">
+    <q-avatar class="absolute-top-right q-ma-md logo" size="60px">
+      <q-img width="30px" src="icons/face-id.svg" />
     </q-avatar>
   </q-card-section>
 </template>
 
 <script setup></script>
+
